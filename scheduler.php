1709864ff399ea2ecfe77e2e622ed520d108dc87
danubise <danubise@gmail.com>
2018-04-30 12:54:58 +0400

    Добавить обработку ввода пинкода

--- scheduler.php
+++ scheduler.php
@@ -14,17 +14,31 @@ $log = new Log($config);
 $log->info("Start");
 
 $agi = new AGI();
-$log->info("Incomming call from :");
-$log->info($agi->request['agi_callerid']);
+$log->info( "Incomming call from :" );
+$log->info( $agi->request['agi_callerid'] );
+//Добавить проверку номера звонящего абонента
 $agi->answer();
-
+//Введите пинкод
 $agi->stream_file("en/demo-congrats","#");
+$try=0;
 do{
-    $agi->stream_file("enter-some-digits","#");
     $result = $agi->get_data('beep', 3000, 20);
     $keys = $result['result'];
+    //Проверка пинкод в базе
+    if($keys == "111" ){
+        $log->info("Pincode successful");
+        break;
+    }
+    $exists = false;
+    //ввден не верный пинкод, повторить попытку
     $agi->stream_file("you-entered","#");
-    $agi->say_digits($keys);
-} while($keys != '111');
+    $log->error("Wrong pincode");
+    $try ++;
+    if($try == 3){
+
+        $agi->Hangup();
+        die;
+    }
+} while(true);
 $log->info("End");
 ?>
