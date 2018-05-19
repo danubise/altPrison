007996cbe5062b67814f88c3420d1d8c497aff31
danubise <danubise@gmail.com>
2018-05-19 20:06:30 +0400

    Добавил отправку ответа почтой

---
+++ test_sendemail.php
@@ -0,0 +1,38 @@
+<?php
+    require_once ("Classes/PHPMailer-master/class.phpmailer.php");
+    $mail['theme']="Тема письмо - отчет";
+    $mail['body']="Тело письма отчет содержиться в прикрепленном файле";
+    $mail['from']="sols@sarfsin.ru";
+    $operatordetail['mail']="danubise@gmail.com";
+
+    $email = new PHPMailer();
+    $email->CharSet = 'UTF-8';
+    $email->From      = $mail['from'];
+    $email->FromName  = $mail['from'];
+    $email->Subject   = $mail['theme'];
+    $email->Body      = $mail['body'];
+    $mails=explode(",",$operatordetail['mail']);
+    foreach($mails as $emailaddress) {
+        $email->AddAddress($emailaddress);
+    }
+    $email->AddAttachment( "test.xls" , "test.xls" );
+    if(!$email->Send()){
+        echo "Message could not be sent. <p>";
+        echo "Mailer Error: " . $email->ErrorInfo;
+        $update = array(
+            "send" => "2",
+            "sentdetail" => "Mailer Error: " . $email->ErrorInfo
+        );
+        //$this->db->update("b_invoicemain",$update, "`invoiceid` = '" . $invoice['maindata']['invoiceid'] . "'");
+        echo "Message has not sent";
+        //die;
+    }else {
+        $update = array(
+            "send" => "1",
+            "sentdetail" => "Message has been sent successful to ". $operatordetail['mail']
+        );
+        //$this->db->update("b_invoicemain", $update, "`invoiceid` = '" . $invoice['maindata']['invoiceid'] . "'");
+        echo "Message has been sent ";
+        echo $operatordetail['mail'] . " sent <br>";
+    }
+?>
