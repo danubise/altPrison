c728c1a6ce7c0462ff74a27eb244181c1a367a47
danubise <danubise@gmail.com>
2018-05-19 19:26:03 +0400

    Добавление класса для формарования отчета, отчет формируется из тестовых данных в эксель документ на двух таблицах, общий и детальный

--- report_class.php
+++ report_class.php
@@ -5,14 +5,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 date_default_timezone_set('Europe/Samara');
-//if (PHP_SAPI == 'cli')
-//	die('This example should only be run from a Web Browser');
-/** Include PHPExcel */
+
 require_once 'Classes/PHPExcel.php';
 include("config.php");
 include("mysqli.php");
 include("log.php");
-$report = new Report($config);
+$report = new Report($config, 17);
 $report->makeReport();
 
 class Report{
@@ -21,23 +19,28 @@ class Report{
     private $db=null;
     private $log=null;
     private $objPHPExcel=null;
+    private $scheduleid=0;
 
-    public function __construct($config=""){
+    public function __construct($config="", $scheduleid){
         $this->config = $config;
-        $this->db = new db($config['mysql']);
+       // $this->db = new db($config['mysql']);
         $this->log = new Log($config);
 
         $this->log->SetGlobalIndex("REPORT");
         $this->log->info( "Start" );
         $this->objPHPExcel = new PHPExcel();
+        $this->scheduleid = $scheduleid;
 
     }
+
     public function makeReport(){
         $this->createFirstSheet();
         $this->createSecondSheet();
         $this->saveReport();
     }
+
     function createFirstSheet(){
+
         $this->objPHPExcel->getProperties()->setCreator("СОЛС")
                                      ->setLastModifiedBy("СОЛС")
                                      ->setTitle("Отчет об оповещении")
@@ -46,26 +49,48 @@ class Report{
                                      ->setKeywords("отчет об оповещении")
                                      ->setCategory("Отчет");
         // Add some data
+        //$firstTotal = $this->db->select("count(*) from `dial` where scheduleid=".$this->scheduleid);
+        //$this->log->debug($this->db->query->last);
+        $firstTotal = 100;
+        //$firstNotAnswered = $this->db->select("count(*) from `dial` where dialcount>1 AND scheduleid=".$this->scheduleid);
+        //$this->log->debug($this->db->query->last);
+        $firstNotAnswered = 20;
+
         $this->objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('B2', 'Круг 1')
                     ->setCellValue('B3', 'Количество номеров')
+                    ->setCellValue('C3', $firstTotal)
                     ->setCellValue('B4', 'Количество неоповещенных')
+                    ->setCellValue('C4', $firstNotAnswered)
                     ->setCellValue('B5', 'Не оповещенные номера');
+
+        $secondTotal = $firstNotAnswered;
+        //$secondNotAnswered = $this->db->select("count(*) from `dial` where dialcount=3 AND scheduleid=".$this->scheduleid);
+        //$this->log->debug($this->db->query->last);
+        $secondNotAnswered = 15;
 
         $this->objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('B7', 'Круг 2')
                     ->setCellValue('B8', 'Количество номеров')
+                    ->setCellValue('C8', $secondTotal)
                     ->setCellValue('B9', 'Количество неоповещенных')
+                    ->setCellValue('C9', $secondNotAnswered)
                     ->setCellValue('B10', 'Не оповещенные номера');
+
+        $thirdTotal = $secondNotAnswered;
+        //$thirdNotAnswered= $this->db->select("count(*) from `dial` where  status=0 AND dialcount=3 AND scheduleid=".$this->scheduleid);
+        //$this->log->debug($this->db->query->last);
+        $thirdNotAnswered = 15;
 
         $this->objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('B12', 'Круг 3')
                     ->setCellValue('B13', 'Количество номеров')
+                    ->setCellValue('C13', $thirdTotal)
                     ->setCellValue('B14', 'Количество неоповещенных')
+                    ->setCellValue('C14', $thirdNotAnswered)
                     ->setCellValue('B15', 'Не оповещенные номера');
 
         $this->objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(25);
-        // Miscellaneous glyphs, UTF-8
 
         $this->objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('A1', 'Общий отчет');
@@ -74,6 +99,17 @@ class Report{
     }
 
     function createSecondSheet(){
+
+        //$numbersForReport = $this->db->select("phonenumber , status , dialcount from dial where scheduleid=".$this->scheduleid);
+        $numbersForReport = array(
+            0 => array("phonenumber" => 6667,"status" => 1,"dialcount" => 1),
+            1 => array("phonenumber" => 6661,"status" => 0,"dialcount" => 3),
+            2 => array("phonenumber" => 6662,"status" => 1,"dialcount" => 2),
+            3 => array("phonenumber" => 6663,"status" => 1,"dialcount" => 3),
+            4 => array("phonenumber" => 6664,"status" => 1,"dialcount" => 3),
+            5 => array("phonenumber" => 6665,"status" => 0,"dialcount" => 3)
+        );
+
         $objWorkSheet = $this->objPHPExcel->createSheet(1);
         $this->objPHPExcel->setActiveSheetIndex(1)
                     ->setCellValue('A1', 'Детальный отчет');
@@ -84,25 +120,31 @@ class Report{
                     ->setCellValue('C1', 'Круг 1')
                     ->setCellValue('D1', 'Круг 2')
                     ->setCellValue('E1', 'Круг 3');
-
-
-
-        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        foreach($numbersForReport as $id=>$numberData){
+            $cellNumber = $id+2;
+            $k1="--";
+            $k2="--";
+            $k3="--";
+            if ($numberData['status'] == 1){
+                if($numberData['dialcount'] == 1){
+                    $k1="OK";
+                }else if($numberData['dialcount'] == 2){
+                    $k2="OK";
+                }else{
+                    $k3="OK";
+                }
+            }
+            $this->objPHPExcel->setActiveSheetIndex(1)
+                            ->setCellValue('A'.$cellNumber, $id+1 )
+                            ->setCellValue('B'.$cellNumber, $numberData['phonenumber'])
+                            ->setCellValue('C'.$cellNumber, $k1)
+                            ->setCellValue('D'.$cellNumber, $k2)
+                            ->setCellValue('E'.$cellNumber, $k3);
+        }
         $this->objPHPExcel->setActiveSheetIndex(0);
-        // Redirect output to a client’s web browser (Excel5)
     }
 
     function saveReport(){
-       // header('Content-Type: application/vnd.ms-excel');
-       // header('Content-Disposition: attachment;filename="01simple.xls"');
-       // header('Cache-Control: max-age=0');
-        // If you're serving to IE 9, then the following may be needed
-      //  header('Cache-Control: max-age=1');
-        // If you're serving to IE over SSL, then the following may be needed
-       // header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-       // header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-       // header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        //header ('Pragma: public'); // HTTP/1.0
         $objWriter = PHPExcel_IOFactory::createWriter($this->objPHPExcel, 'Excel5');
         $objWriter->save('test.xls');
     }
